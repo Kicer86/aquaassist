7ead4b5b8553f5f8b65024433a8eb19d0841aa4d
Michał Walenciak <Kicer86@gmail.com>
2017-05-03 17:35:00 +0200

    Introducing EventModel

---
+++ src/events_model.hpp
@@ -0,0 +1,35 @@
+/*
+ * Model for list of events
+ * Copyright (C) 2017  Michał Walenciak <Kicer86@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef EVENTS_MODEL_HPP
+#define EVENTS_MODEL_HPP
+
+#include <QAbstractItemModel>
+
+class EventsModel : public QAbstractItemModel
+{
+    public:
+        EventsModel();
+        EventsModel(const EventsModel &) = delete;
+        ~EventsModel();
+        
+        EventsModel& operator=(const EventsModel &) = delete;
+};
+
+#endif // EVENTS_MODEL_HPP
